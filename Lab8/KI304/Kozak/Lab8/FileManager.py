645e1a2419af6de8b38849179e607fbf934573ed
MaksGMaks <maksgmaks.yt@gmail.com>
2024-11-13 13:22:13 +0200

    Lab8 is ready

--- Lab8/KI304/Kozak/Lab8/FileManager.py
+++ Lab8/KI304/Kozak/Lab8/FileManager.py
@@ -1,9 +1,14 @@
 import struct
 
 
+"""
+Clears the content of the text file "result.txt" by opening it in write mode 
+and closing it immediately.
+"""
 def clear_txt():
     fname = "result.txt"
     try:
+        # Open the file in text write mode ('t+w') with UTF-8 encoding
         file = open(fname, "t+w", encoding="utf-8")
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
@@ -11,9 +16,15 @@ def clear_txt():
     finally:
         file.close()
 
+
+"""
+Clears the content of the binary file "result.bin" by opening it in write mode
+and closing it immediately.
+"""
 def clear_bin():
     fname = "result.bin"
     try:
+        # Open the file in binary write mode ('b+w')
         file = open(fname, "b+w")
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
@@ -21,44 +32,70 @@ def clear_bin():
     finally:
         file.close()
 
+
+"""
+Appends a message to the text file "result.txt".
+
+Parameters:
+message (str): The message to be written to the text file.
+"""
 def write_txt(message):
     fname = "result.txt"
     try:
+        # Open the file in append mode ('t+a') with UTF-8 encoding
         file = open(fname, "t+a", encoding="utf-8")
-        file.write(message)
+        file.write(message)  # Write the message to the file
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
         exit(-1)
     finally:
         file.close()
 
+
+"""
+Appends a message to the binary file "result.bin". The message is encoded in UTF-8.
+
+Parameters:
+message (str): The message to be written to the binary file.
+"""
 def write_bin(message):
     fname = "result.bin"
     try:
+        # Open the file in binary append mode ('b+a')
         with open(fname, 'b+a') as file:
-            file.write(message.encode('utf-8'))
+            file.write(message.encode('utf-8'))  # Encode and write the message
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
         exit(-1)
 
+
+"""
+Reads and prints the contents of the text file "result.txt".
+"""
 def read_txt():
     fname = "result.txt"
     try:
+        # Open the file in text read mode ('t+r') with UTF-8 encoding
         file = open(fname, "t+r", encoding="utf-8")
         for line in file:
-            print(line, end='')
+            print(line, end='')  # Print each line of the file
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
         exit(-1)
     finally:
         file.close()
 
+
+"""
+Reads and prints the contents of the binary file "result.bin", decoding the data as UTF-8.
+"""
 def read_bin():
     fname = "result.bin"
     try:
+        # Open the file in binary read mode ('b+r')
         with open(fname, 'b+r') as file:
             for line in file:
-                print(line.decode('utf-8'), end='')
+                print(line.decode('utf-8'), end='')  # Decode each binary line and print it
     except OSError:
         print(f"Error while opening file {fname}. Program terminated")
         exit(-1)
